cbb04527be228ba9394608836f9cb6738357824e
Marijoy S.Novora <msn2022-2479-78090@bicol-u.edu.ph>
2026-05-11 13:17:12 +0800

    time in

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("JS WORKING");
 
     let required = 486;
-    let logged = 398;
+    let logged = 408;
 
     let percent = Math.round((logged / required) * 100);
     let remaining = required - logged;
